51ea21f5d93533f63a8694f159f8692658c967d8
AcMamaw <usakamillion@gmail.com>
2026-03-07 20:27:17 +0800

    fix: SES region us-east-1 -> ap-southeast-2

--- config/services.php
+++ config/services.php
@@ -23,15 +23,15 @@ return [
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'region' => env('AWS_DEFAULT_REGION', 'ap-southeast-2'), // ✅ FIXED: was 'us-east-1'
     ],
 
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'channel'              => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
 
