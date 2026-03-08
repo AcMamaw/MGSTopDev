d5d4e0f23614a33b2526ff4841898a10253be28b
AcMamaw <usakamillion@gmail.com>
2026-03-08 18:46:59 +0800

    Fix S3 region hardcoded to ap-southeast-2

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,24 +48,24 @@ return [
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'driver'   => 's3',
+            'key'      => env('AWS_ACCESS_KEY_ID'),
+            'secret'   => env('AWS_SECRET_ACCESS_KEY'),
+            'region'   => 'ap-southeast-2', // ← HARDCODED
+            'bucket'   => env('AWS_BUCKET'),
+            'url'      => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'use_path_style_endpoint' => false, // ← HARDCODED
             'throw' => false,
             'report' => false,
         ],
 
         'cloudinary' => [
-            'driver' => 'cloudinary',
+            'driver'     => 'cloudinary',
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_key'    => env('CLOUDINARY_API_KEY'),
             'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'secure' => true,
+            'secure'     => true,
         ],
 
     ],
@@ -86,4 +86,3 @@ return [
     ],
 
 ];
-
